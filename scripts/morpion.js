6c50958c7d08273fd7dfd026ffe7d75e4bbb7d96
Morgan <morgan.gillette@ecole.ensicaen.fr>
2024-01-19 15:08:54 +0100

    heigth fix

--- scripts/morpion.js
+++ scripts/morpion.js
@@ -28,8 +28,14 @@ function newMorpion() {
             mp.drawShape(mp.currentPlayer, x, y);
             mp.grid[x][y] = mp.currentPlayer;
             mp.nextPlayer();
-            mp.printInfo("Tour : " + mp.currentPlayer);
             mp.freeCells -= 1;
+
+            if (mp.freeCells == 0) {
+                mp.printInfo("Partie terminée");
+            } else {
+                mp.printInfo("Tour : " + mp.currentPlayer);
+            }
+
         } else {
             mp.printInfo("Tour : " + mp.currentPlayer + "<br>Vous ne pouvez pas jouer ici !");
         }
@@ -77,7 +83,7 @@ class Morpion {
 
     drawGrid() {
         this.mp_cv_ct.beginPath();
-        this.mp_cv_ct.lineWidth = 6;
+        this.mp_cv_ct.lineWidth = 5;
         this.mp_cv_ct.strokeStyle = "grey";
 
         this.mp_cv_ct.moveTo(100.5, 0);
@@ -88,14 +94,12 @@ class Morpion {
         this.mp_cv_ct.lineTo(200.5, 300.5);
         this.mp_cv_ct.stroke();
 
-        this.mp_cv_ct.lineWidth = 3;
-
         this.mp_cv_ct.moveTo(0, 100.5);
         this.mp_cv_ct.lineTo(300.5, 100.5);
         this.mp_cv_ct.stroke();
 
-        this.mp_cv_ct.moveTo(0, 50.5);
-        this.mp_cv_ct.lineTo(300.5, 50.5);
+        this.mp_cv_ct.moveTo(0, 200.5);
+        this.mp_cv_ct.lineTo(300.5, 200.5);
         this.mp_cv_ct.stroke();
 
         this.mp_cv_ct.closePath();
@@ -112,18 +116,18 @@ class Morpion {
         if (shape == "X") {
 
             x = 101 * x + 12;
-            y = 51 * y + 7;
+            y = 101 * y + 12;
 
             this.mp_cv_ct.beginPath();
             this.mp_cv_ct.lineWidth = 6;
             this.mp_cv_ct.strokeStyle = "grey";
 
             this.mp_cv_ct.moveTo(x, y);
-            this.mp_cv_ct.lineTo(x + 75, y + 35);
+            this.mp_cv_ct.lineTo(x + 75, y + 75);
             this.mp_cv_ct.stroke();
 
             this.mp_cv_ct.moveTo(x + 75, y);
-            this.mp_cv_ct.lineTo(x, y + 35);
+            this.mp_cv_ct.lineTo(x, y + 75);
             this.mp_cv_ct.stroke();
 
             this.mp_cv_ct.closePath();
@@ -132,13 +136,13 @@ class Morpion {
         } else if (shape == "O") {
 
             x = 101 * x + 50;
-            y = 51 * y + 24;
+            y = 101 * y + 50;
 
             this.mp_cv_ct.beginPath();
             this.mp_cv_ct.lineWidth = 6;
             this.mp_cv_ct.strokeStyle = "grey";
 
-            this.mp_cv_ct.ellipse(x, y, 34, 17, 0, 0, 2 * Math.PI);
+            this.mp_cv_ct.ellipse(x, y, 38, 38, 0, 0, 2 * Math.PI);
             this.mp_cv_ct.stroke();
 
             this.mp_cv_ct.closePath();
